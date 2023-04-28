5ebd50c393b45804ca279ddb2e76f4b5a5af3e08
AndreaNikolchova <a.nikolchova04@gmail.com>
2023-04-28 17:37:29 +0300

    New problems

---
+++ Structures of data and algorithums/Stuctures of data and algorithums/2.3.2/Program.cs
@@ -0,0 +1,63 @@
+﻿class Program
+{
+    public static double sum;
+    public static int count = 0;
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        int x = int.Parse(Console.ReadLine()!);
+        Recursion(n);
+    }
+    public static void SqrtRecursion(int n)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine(sum);
+            return;
+        }
+        if (count == 0)
+        {
+            sum = Math.Sqrt(n);
+        }
+        else
+        {
+
+            sum = Math.Sqrt(n + sum);
+        }
+        count++;
+        SqrtRecursion(n - 1);
+    }
+    public static void RationalsRecursion(int n,int x)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine(sum+1);
+            return;
+        }
+        sum+= 1/Math.Pow(x,n);
+        RationalsRecursion(n - 1,x);
+    }
+
+    public static void PowRecursion(int n,int x)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine(sum + 1);
+            return;
+        }
+        sum += Math.Pow(x, n);
+        PowRecursion(n - 1, x);
+    }
+    public static void Recursion(int n)
+    {
+        if(n == 0)
+        {
+            Console.WriteLine(sum);
+            return;
+        }
+        sum += n*(n-1);
+        Recursion(n-1);
+    }
+
+
+}
---
+++ Structures of data and algorithums/Stuctures of data and algorithums/2.4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<int> list = new List<int>();
+
+for (int i = 0; i < n; i++)
+{
+    list.Add(int.Parse(Console.ReadLine()));
+}
+
+list.Sort();
+
+int min = 1000;
+
+for (int i = 0; i < n - 2; i++)
+{
+    if (min > Math.Abs(list[i] - list[i + 1]))
+    {
+        min = Math.Abs(list[i] - list[i + 1]);
+    }
+}
+Console.WriteLine(min);
